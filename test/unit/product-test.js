542b05be74d95d9d317a5166e7e2f13ca10e2135
Alex <alex@scotttonge.com>
2017-12-06 16:02:48 +0000

    unit test for saving product to database

--- test/unit/product-test.js
+++ test/unit/product-test.js
@@ -1,14 +1,20 @@
-// describe ('Product Unit Tests', () => {
-//   describe('#create()', () => {
-//     it('should create a new product', (done) => {
-//       Product.create({
-//         name: 'milk',
-//         barcode: 123,
-//       }).then((product) => {
-//         // assert(product)
-//         done()
-//       })
-//
-//     })
-//   })
-// })
+const models  = require('../../models');
+const chai = require ('chai');
+const expect = chai.expect;
+// const Product = require('../../models/product')
+
+
+describe ('Product Unit Tests', () => {
+  describe('#create()', () => {
+    it('should create a new product', (done) => {
+      models.Product.create({
+        name: 'milk',
+        barcode: 123,
+      }).then((product) => {
+        expect(product).to.include({name: 'milk'})
+        done()
+      })
+
+    })
+  })
+})
