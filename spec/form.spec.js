cce55720a9eb3cf2eff10b6fc9c8511499da305c
Alex <alex@scotttonge.com>
2017-12-06 12:42:10 +0000

    test structure for jest and puppeteer

---
+++ spec/form.spec.js
@@ -0,0 +1,36 @@
+import faker from "faker";
+import puppeteer from "puppeteer";
+const APP = "localhost:3000";
+
+const product = {
+  name: "bobby",
+  barcode: 1234,
+};
+
+let page;
+let browser;
+const width = 1920;
+const height = 1080;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: [`--window-size=${width},${height}`]
+  });
+  page = await browser.newPage();
+  await page.setViewport({ width, height });
+  await page.goto("http://localhost:3000/")
+});
+afterAll(() => {
+  browser.close();
+});
+
+describe('Product form', () => {
+  test("user can submit a product which will display in list", async () => {
+    await page.type("input[name=name]", product.name);
+    await page.type("input[name=barcode]", product.barcode);
+    await page.click("button[type=submit]");
+    expect(page).toContain(product.name);
+  })
+})
---
+++ spec/spec_helper.js
@@ -0,0 +1,12 @@
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: [`--window-size=${width},${height}`]
+  });
+  page = await browser.newPage();
+  await page.setViewport({ width, height });
+});
+afterAll(() => {
+  browser.close();
+});
