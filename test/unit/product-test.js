ac9067a779452054a4d7aba3f5a718ad628c62cc
Alex <alex@scotttonge.com>
2017-12-06 15:43:48 +0000

    setup folder structure for unit testing

---
+++ test/unit/product-test.js
@@ -0,0 +1,14 @@
+// describe ('Product Unit Tests', () => {
+//   describe('#create()', () => {
+//     it('should create a new product', (done) => {
+//       Product.create({
+//         name: 'milk',
+//         barcode: 123,
+//       }).then((product) => {
+//         // assert(product)
+//         done()
+//       })
+//
+//     })
+//   })
+// })
