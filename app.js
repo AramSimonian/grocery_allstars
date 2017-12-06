522888ec36311d3ab697ec135d8eff4e90fcc4a1
Pablo <paul.ladiv123@gmail.com>
2017-12-06 14:51:06 +0000

    rename routes : now named controllers

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var products = require('./routes/products');
-var users = require('./routes/users');
+var index = require('./controllers/index');
+var products = require('./controllers/products');
+var users = require('./controllers/users');
 
 var app = express();
 
