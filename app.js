9918ff1c6ac880f81e7d5db60bf218a766a6b1d9
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-12-09 17:51:22 +0000

    Various styling.

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+var engine = require('ejs-layout');
 app.set('view engine', 'ejs');
+app.engine('ejs', engine.__express);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
