a7dd4815bc2a13ee8910b62a0655650f99d5d888
Aram Simonian <aram.contacts@gmail.com>
2017-12-11 14:33:25 +0000

    Created first basic test for userProducts and associated migration script

---
+++ test/unit/user-product-test.js
@@ -0,0 +1,36 @@
+const models = require('../../models');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('User Products Unit Tests', () => {
+
+  before(function(done) {
+    models.User.sync({ force : true }) // drops table and re-creates it
+      .then(function() {
+        done(null);
+      })
+      .error(function(error) {
+        done(error);
+      });
+  });
+
+  describe('#create()', () => {
+    it('should create a new user product record', (done) => {
+      dateNow = Date.now();
+      models.UserProducts.create({
+        userId: 1,
+        productId: 1,
+        expiryDate: dateNow,
+        status: '',
+      }).then((userProduct) => {
+        expect(userProduct).to.include({userId: 1});
+        expect(userProduct).to.include({productId: 1});
+        expect(userProduct).to.include({expiryDate: dateNow});
+        expect(userProduct).to.include({status: ''});
+        done();
+      })
+    });
+
+
+  }) // secondary describe
+}); // maine describe
