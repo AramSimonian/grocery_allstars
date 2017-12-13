462282e33c1c4a7cca3f6f83e2343363f23c2ffe
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-12-13 12:55:29 +0000

    Fixed callback for before product testing.

--- test/feature/product-test.js
+++ test/feature/product-test.js
@@ -4,25 +4,26 @@ const chai = require('chai');
 const expect = chai.expect;
 const http = require('http');
 const assert = chai.assert;
+var knex      = require('../../models/database').knex;
+var bookshelf = require('bookshelf')(knex);
+var knexCleaner = require('knex-cleaner');
 
 describe.only('Product feature testing', () => {
 
   before((done) => {
+    knexCleaner.clean(bookshelf.knex);
     this.server = http.createServer(app).listen(3000);
     this.browser = new Browser({
       site: 'http://localhost:3000'
     });
-    this.browser.visit('/auth/register').then((done) => {
+    this.browser.visit('/auth/register').then(() => {
       this.browser.fill('input[name=email]', 'test@test.com');
       this.browser.fill('input[name=password]', 'password');
       this.browser.fill('input[name=firstName]', 'Test');
       this.browser.fill('input[name=lastName]', 'User');
-      this.browser.pressButton('Sign up').then(() => {
-        expect(this.browser.text('div')).to.match(/Test Name/);
-        done();
-      });
+      this.browser.pressButton('Sign up');
+      done();
     });
-    done()
   });
 
   beforeEach((done) => {
@@ -40,6 +41,7 @@ describe.only('Product feature testing', () => {
 
   describe('Add a product', () => {
     it('displays added product on page', (done) => {
+      console.log(this.browser);
       // console.log(this.browser.HTMLInputElement())
       this.browser.fill('input[name=name]', 'Test Name');
       this.browser.fill('input[name=barcode]', '12345');
